7f308fc698d5d2d10ac739770944d60733f0ae2e
nannige1304 <nannige1304@gmail.com>
2025-09-01 16:45:01 +0800

    nanige

---
+++ learnJS1/script.js
@@ -0,0 +1,14 @@
+let money=500/500;
+let price=99.99;
+let message ='nannige';
+let name= "nannige";
+let checkname =false;
+let checkproduct =true
+
+console.log(typeof(money));
+console.log(typeof(price));
+console.log(typeof(message));
+console.log(typeof(name));
+
+console.log(checkname);
+console.log(checkproduct);  
